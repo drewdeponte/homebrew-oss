18d288db37b5965205ddf20d384b40df68a54c3e
Andrew De Ponte <cyphactor@gmail.com>
2016-07-22 13:45:12 -0700

    Update to reference codebreakdown path
    Why you made the change:
    
    I did this so that it wouldn't have to use the redirects which might only work
    for some temporary amount of time.

--- Formula/alt.rb
+++ Formula/alt.rb
@@ -1,7 +1,7 @@
 class Alt < Formula
   desc "command-line utility to find alternate file"
-  homepage "https://github.com/cyphactor/alt"
-  url "https://github.com/cyphactor/alt/releases/download/v2.0.1/alt-2.0.1-x86_64-apple-darwin"
+  homepage "https://github.com/codebreakdown/alt"
+  url "https://github.com/codebreakdown/alt/releases/download/v2.0.1/alt-2.0.1-x86_64-apple-darwin"
   version "2.0.1"
   sha256 "d472c7ee484c7f5f3320a55b5a73ef6b37599c7b90e0e5b90bfc8a25d36369b4"
 
