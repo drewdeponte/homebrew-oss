e7d7a05c0f38f70a43126b6dc6cd20cb108b7253
Andrew De Ponte <cyphactor@gmail.com>
2017-06-15 22:08:29 -0700

    Upgrade alt Formula to v2.1.1

--- Formula/alt.rb
+++ Formula/alt.rb
@@ -1,12 +1,12 @@
 class Alt < Formula
   desc "command-line utility to find alternate file"
   homepage "https://github.com/uptech/alt"
-  url "https://github.com/uptech/alt/releases/download/v2.1.0/alt-2.1.0-x86_64-apple-darwin"
-  version "2.1.0"
-  sha256 "9bbc088de8d74b3c2788163161958ef986f499ef13e8ec242f6eb5bec06d7da6"
+  url "https://github.com/uptech/alt/releases/download/v2.1.1/alt-2.1.1-x86_64-apple-darwin"
+  version "2.1.1"
+  sha256 "5c783dbba97d85d73663eeac185dd15a5290c2267502dd7820817266d01e8802"
 
   def install
-    mv "alt-2.1.0-x86_64-apple-darwin", "alt"
+    mv "alt-2.1.1-x86_64-apple-darwin", "alt"
     bin.install "alt"
   end
 
