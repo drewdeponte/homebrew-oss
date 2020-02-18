139cd3814119303e5128175e3d2b2db79f30600e
Andrew De Ponte <cyphactor@gmail.com>
2020-02-18 11:44:49 -0800

    Release alt v3.2.0

--- Formula/alt.rb
+++ Formula/alt.rb
@@ -1,17 +1,18 @@
 class Alt < Formula
   desc "command-line utility to find alternate file"
   homepage "https://github.com/uptech/alt"
-  url "https://github.com/uptech/alt/archive/v3.1.0.tar.gz"
-  version "3.1.0"
-  sha256 "8e308dacd2fe6ff3b4a5721b79ed65b140ca8c04d0741506342534fa3664a6c5"
+  url "https://github.com/uptech/alt/archive/v3.2.0.tar.gz"
+  version "3.2.0"
+  sha256 "008d33911305de5359d8976d197618aa5302ac556f7082db84faf6554cfd9c56"
   head "https://github.com/uptech/alt.git"
 
   depends_on "rust" => :build
 
   bottle do
+    # root_url "https://homebrew.bintray.com/bottles-oss"
     root_url "https://github.com/uptech/homebrew-oss/raw/master/Formula"
     cellar :any_skip_relocation
-    sha256 "a86c08a2fe61c3ae7ca4225a97cb8b7f31635e97407e082836f4cec8083b8116" => :mojave
+    sha256 "316ba6db772781d150abeee7bf7abb0ed8b2fd6faf19d1d01c0760f5f44268f4" => :catalina
   end
 
   def install
