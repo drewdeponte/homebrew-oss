e1a2deac4b6a80e04fa847652d64d9b1f270b77e
Andrew De Ponte <cyphactor@gmail.com>
2020-04-24 13:56:59 -0700

    Release v1.1.0 of git-changelog
    I did this so that people would be able to upgrade to it and install it
    from Homebrew.

--- Formula/git-changelog.rb
+++ Formula/git-changelog.rb
@@ -1,7 +1,7 @@
 class GitChangelog < Formula
   desc "Git Changelog generation CLI extension for Git"
   homepage "https://github.com/uptech/git-changelog"
-  url "https://github.com/uptech/git-changelog.git", :tag => "v1.0.0", :revision => "3e27a81d0bb8f7a96f19a804a59f78bed94dee60"
+  url "https://github.com/uptech/git-changelog.git", :tag => "v1.1.0", :revision => "12828a57f8a8e71bcf18097227badf80b84d4951"
   head "https://github.com/uptech/git-changelog.git"
 
   depends_on :xcode => ["10.13", :build]
