836eaf0b6d563e7577341c6388b63d6340a15386
Andrew De Ponte <cyphactor@gmail.com>
2020-04-27 12:25:21 -0700

    Fix the git-cl formula
    I neglected to name the class appropriately so it wasn't able to
    actually install.

--- Formula/git-cl.rb
+++ Formula/git-cl.rb
@@ -1,4 +1,4 @@
-class GitChangelog < Formula
+class GitCl < Formula
   desc "Git Changelog management CLI extension for Git"
   homepage "https://github.com/uptech/git-cl"
   url "https://github.com/uptech/git-cl.git", :tag => "v2.0.0", :revision => "591d7c146631a4d4645fa5a4fb4d8e3eed3affae"
