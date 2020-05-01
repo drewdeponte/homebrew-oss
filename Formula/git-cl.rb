a2acc5e9010f957485c2ba09482cf342ba345c36
Andrew De Ponte <cyphactor@gmail.com>
2020-05-01 01:14:10 -0700

    Release git-cl v3.1.2

--- Formula/git-cl.rb
+++ Formula/git-cl.rb
@@ -1,7 +1,7 @@
 class GitCl < Formula
   desc "Git Changelog management CLI extension for Git"
   homepage "https://github.com/uptech/git-cl"
-  url "https://github.com/uptech/git-cl.git", :tag => "v3.1.1", :revision => "13e87891f3bcf7ba929004cefff80db6c37fd471"
+  url "https://github.com/uptech/git-cl.git", :tag => "v3.1.2", :revision => "3dd7ddf73d993b6522309f4141af777b6f403614"
   head "https://github.com/uptech/git-cl.git"
 
   depends_on :xcode => ["10.13", :build]
