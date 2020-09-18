f5ea702c9a9d656dd302ccd35d22c3fdf93a6de5
Andrew De Ponte <cyphactor@gmail.com>
2020-09-18 14:15:06 -0400

    Release git-ps 0.4.0

--- Formula/git-ps.rb
+++ Formula/git-ps.rb
@@ -1,7 +1,7 @@
 class GitPs < Formula
   desc "Patch Stack workflow CLI extension for Git"
   homepage "https://github.com/uptech/git-ps"
-  url "https://github.com/uptech/git-ps.git", :tag => "0.3.4", :revision => "3cb8874bbbbd982bb8745b5284d87dcdfb3f5fda"
+  url "https://github.com/uptech/git-ps.git", :tag => "0.4.0", :revision => "7ce05dd37b8e21ff77a15f279c158c430682607a"
   head "https://github.com/uptech/git-ps.git"
 
   depends_on :xcode => ["10.13", :build]
