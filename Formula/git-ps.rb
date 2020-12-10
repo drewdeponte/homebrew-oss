2376b02cf172c30f3c517e85ad2d0a31aaee4aac
Andrew De Ponte <cyphactor@gmail.com>
2020-12-10 11:17:48 -0500

    Add git-ps 0.5.0 catalina bottle

--- Formula/git-ps.rb
+++ Formula/git-ps.rb
@@ -10,6 +10,7 @@ class GitPs < Formula
     root_url "https://github.com/uptech/homebrew-oss/raw/master/Formula"
     cellar :any_skip_relocation
     sha256 "cab22bc2932817c9255bc6ba73a9cf9f4787eb58d5a6244f061e9be5ec0bc801" => :big_sur
+    sha256 "cab22bc2932817c9255bc6ba73a9cf9f4787eb58d5a6244f061e9be5ec0bc801" => :catalina
   end
 
   def install
