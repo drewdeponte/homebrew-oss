29a82cf747a87e63afbf827e5b4a12fcef214051
Andrew De Ponte <cyphactor@gmail.com>
2020-12-10 02:24:24 -0500

    Update git-ps to 0.5.0 for src builds

--- Formula/git-ps.rb
+++ Formula/git-ps.rb
@@ -1,7 +1,7 @@
 class GitPs < Formula
   desc "Patch Stack workflow CLI extension for Git"
   homepage "https://github.com/uptech/git-ps"
-  url "https://github.com/uptech/git-ps.git", :tag => "0.4.0", :revision => "7ce05dd37b8e21ff77a15f279c158c430682607a"
+  url "https://github.com/uptech/git-ps.git", :tag => "0.5.0", :revision => "99b8fad74be4b05e7cce37b9eebea07c227043bd"
   head "https://github.com/uptech/git-ps.git"
 
   depends_on :xcode => ["10.13", :build]
