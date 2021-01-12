11ac7b6c4bf84b586a51f8d1d7daeaf6b4cbd19a
Andrew De Ponte <cyphactor@gmail.com>
2021-01-12 00:27:54 -0500

    Publish git-cl v3.3.2
    So that people will be able to easily install this version of git-cl.

--- Formula/git-cl.rb
+++ Formula/git-cl.rb
@@ -1,7 +1,7 @@
 class GitCl < Formula
   desc "Git Changelog management CLI extension for Git"
   homepage "https://github.com/uptech/git-cl"
-  url "https://github.com/uptech/git-cl.git", :tag => "v3.3.1", :revision => "1aebdcb00d889b9a2f5e20109f4782c5f66ce8a7"
+  url "https://github.com/uptech/git-cl.git", :tag => "v3.3.2", :revision => "1fc02e9a3e0a4ce47c95284ed7d2f598e12a5547"
   head "https://github.com/uptech/git-cl.git"
 
   depends_on :xcode => ["10.13", :build]
