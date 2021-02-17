7178330f884d748f2dbd5896d6536ff591211450
Andrew De Ponte <cyphactor@gmail.com>
2021-02-17 14:59:31 -0500

    Fix sha of git-ps 0.6.0

--- Formula/git-ps.rb
+++ Formula/git-ps.rb
@@ -1,7 +1,7 @@
 class GitPs < Formula
   desc "Patch Stack workflow CLI extension for Git"
   homepage "https://github.com/uptech/git-ps"
-  url "https://github.com/uptech/git-ps.git", :tag => "0.6.0", :revision => "c7480907ce8095ee4cf8f1d9c9fac7e1eb3af936"
+  url "https://github.com/uptech/git-ps.git", :tag => "0.6.0", :revision => "c455a680fff7ad85f7f51e5fb18b3e0ec9f03a4f"
   head "https://github.com/uptech/git-ps.git"
 
   depends_on :xcode => ["10.13", :build]
