632fe1aaf0744f5f59ed3add5dbb1a504e31fbdd
Andreas Wachowski <andreas.wachowski@gmail.com>
2021-04-16 19:31:33 +0200

    Run brew style fix on alt.rb
    $ brew style --fix alt.rb
    alt.rb:2:9: C: [Corrected] Description should start with a capital letter.
      desc "command-line utility to find alternate file"
            ^
    alt.rb:11:3: C: [Corrected] bottle (line 11) should be put before depends_on (line 9)
      bottle do ...
      ^^^^^^^^^
    alt.rb:12:5: C: [Corrected] cellar should be a parameter to sha256
        cellar :any_skip_relocation
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    alt.rb:14:5: C: [Corrected] cellar should be a parameter to sha256
        cellar :any_skip_relocation
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    alt.rb:14:5: C: [Corrected] sha256 should use new syntax
        sha256 "33084423ae8a9b5d19544f7eef12a6ceca1e95eee568366ccb5f1a208a7d5710" => :catalina
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    alt.rb:16:5: C: [Corrected] sha256 should use new syntax
        sha256 "33084423ae8a9b5d19544f7eef12a6ceca1e95eee568366ccb5f1a208a7d5710" => :catalina
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    alt.rb:19:5: C: use "cargo", "install", *std_cargo_args
        system "cargo", "build", "--release"
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    1 file inspected, 7 offenses detected, 6 offenses corrected

--- Formula/alt.rb
+++ Formula/alt.rb
@@ -1,20 +1,19 @@
 class Alt < Formula
-  desc "command-line utility to find alternate file"
+  desc "Command-line utility to find alternate file"
   homepage "https://github.com/uptech/alt"
   url "https://github.com/uptech/alt/archive/v3.3.0.tar.gz"
   version "3.3.0"
   sha256 "d543d102165bd4f74a60d259fb292028ab111b1051b1403842019db351601a78"
   head "https://github.com/uptech/alt.git"
 
-  depends_on "rust" => :build
-
   bottle do
     # root_url "https://homebrew.bintray.com/bottles-oss"
     root_url "https://github.com/uptech/homebrew-oss/raw/master/Formula"
-    cellar :any_skip_relocation
     rebuild 1
-    sha256 "33084423ae8a9b5d19544f7eef12a6ceca1e95eee568366ccb5f1a208a7d5710" => :catalina
+    sha256 cellar: :any_skip_relocation, catalina: "33084423ae8a9b5d19544f7eef12a6ceca1e95eee568366ccb5f1a208a7d5710"
   end
+
+  depends_on "rust" => :build
 
   def install
     system "cargo", "build", "--release"
