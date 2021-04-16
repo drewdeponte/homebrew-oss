6da7dd266604a7004b10edbeb5b55f90dffdda8f
Andreas Wachowski <andreas.wachowski@gmail.com>
2021-04-16 19:35:01 +0200

    Run brew style fix on git-ps.rb
    $ brew style --fix git-ps.rb
    git-ps.rb:4:47: C: [Corrected] Use the new Ruby 1.9 hash syntax.
      url "https://github.com/uptech/git-ps.git", :tag => "0.6.0", :revision => "c455a680fff7ad85f7f51e5fb18b3e0ec9f03a4f"
                                                  ^^^^^^^
    git-ps.rb:4:64: C: [Corrected] Use the new Ruby 1.9 hash syntax.
      url "https://github.com/uptech/git-ps.git", :tag => "0.6.0", :revision => "c455a680fff7ad85f7f51e5fb18b3e0ec9f03a4f"
                                                                   ^^^^^^^^^^^^
    git-ps.rb:7:14: C: [Corrected] Use the new Ruby 1.9 hash syntax.
      depends_on :xcode => ["10.13", :build]
                 ^^^^^^^^^
    git-ps.rb:9:3: C: [Corrected] bottle (line 9) should be put before depends_on (line 7)
      bottle do ...
      ^^^^^^^^^
    git-ps.rb:9:5: C: [Corrected] cellar should be a parameter to sha256
        cellar :any_skip_relocation
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    git-ps.rb:9:51: C: [Corrected] Align bottle digests
        sha256 cellar: :any_skip_relocation, big_sur: "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3"
                                                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    git-ps.rb:10:5: C: [Corrected] sha256 should use new syntax
        sha256 "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3" => :big_sur
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    git-ps.rb:11:5: C: [Corrected] cellar should be a parameter to sha256
        cellar :any_skip_relocation
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    git-ps.rb:11:5: C: [Corrected] sha256 should use new syntax
        sha256 "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3" => :catalina
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    git-ps.rb:12:5: C: [Corrected] sha256 should use new syntax
        sha256 "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3" => :big_sur
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    git-ps.rb:13:5: C: [Corrected] sha256 should use new syntax
        sha256 "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3" => :catalina
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    git-ps.rb:22:12: C: [Corrected] Separate system commands into "#{bin}/git-ps", "--version"
        system "#{bin}/git-ps --version"
               ^^^^^^^^^^^^^^^^^^^^^^^^^
    
    1 file inspected, 12 offenses detected, 12 offenses corrected

--- Formula/git-ps.rb
+++ Formula/git-ps.rb
@@ -1,17 +1,16 @@
 class GitPs < Formula
   desc "Patch Stack workflow CLI extension for Git"
   homepage "https://github.com/uptech/git-ps"
-  url "https://github.com/uptech/git-ps.git", :tag => "0.6.0", :revision => "c455a680fff7ad85f7f51e5fb18b3e0ec9f03a4f"
+  url "https://github.com/uptech/git-ps.git", tag: "0.6.0", revision: "c455a680fff7ad85f7f51e5fb18b3e0ec9f03a4f"
   head "https://github.com/uptech/git-ps.git"
-
-  depends_on :xcode => ["10.13", :build]
 
   bottle do
     root_url "https://github.com/uptech/homebrew-oss/raw/master/Formula"
-    cellar :any_skip_relocation
-    sha256 "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3" => :big_sur
-    sha256 "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3" => :catalina
+    sha256 cellar: :any_skip_relocation, big_sur:  "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3"
+    sha256 cellar: :any_skip_relocation, catalina: "054d1cb6f68c317d7c92b23c61341627d2316eb723466f476baa82945fbb46f3"
   end
+
+  depends_on xcode: ["10.13", :build]
 
   def install
     system "make", "build"
@@ -19,6 +18,6 @@ class GitPs < Formula
   end
 
   test do
-    system "#{bin}/git-ps --version"
+    system "#{bin}/git-ps", "--version"
   end
 end
