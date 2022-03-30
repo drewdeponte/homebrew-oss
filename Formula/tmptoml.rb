218ad59597375dbfb5d13410f99ac85047c451ce
ChillBroDev <jon@upte.ch>
2022-03-30 13:59:49 -0700

    Fix malformed tmptoml url in formulae
    ps-id: 390AA451-B309-4AF6-8272-06298C4E01AE

--- Formula/tmptoml.rb
+++ Formula/tmptoml.rb
@@ -1,7 +1,7 @@
 class TmpToml < Formula
   desc "Command-line utility to render Tera templates using a toml config file as the variable source."
   homepage "https://github.com/uptech/tmptoml"
-  url "https://github.com/uptech/tmptoml.git" tag: "0.1.3", revision: "f4faf718dddc00b1e31dcb455fe0c28bca1fc574"
+  url "https://github.com/uptech/tmptoml.git", tag: "0.1.3", revision: "f4faf718dddc00b1e31dcb455fe0c28bca1fc574"
   version "0.1.3"
   head "https://github.com/uptech/tmptoml.git"
 
