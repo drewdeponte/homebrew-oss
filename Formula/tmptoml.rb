a96658e300ace6b1c85d08c153206b64382d5078
ChillBroDev <jon@upte.ch>
2022-03-30 13:19:36 -0700

    Add Tmptoml v0.1.3 bottle release
    ps-id: 8942449E-57CA-4419-ABD6-40BDBBBE8E0D

---
+++ Formula/tmptoml.rb
@@ -0,0 +1,20 @@
+class TmpToml < Formula
+  desc "Command-line utility to render Tera templates using a toml config file as the variable source."
+  homepage "https://github.com/uptech/tmptoml"
+  url "https://github.com/uptech/tmptoml.git" tag: "0.1.3", revision: "f4faf718dddc00b1e31dcb455fe0c28bca1fc574"
+  version "0.1.3"
+  head "https://github.com/uptech/tmptoml.git"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+
+    bin.install "target/release/tmptoml"
+    # man1.install "doc/tmptoml.1" # only in versions > 2.3.0
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tmptoml --version")
+  end
+end
