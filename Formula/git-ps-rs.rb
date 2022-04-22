53e8ed03d1d312b47ad06e6718fe647ce773eef3
Drew De Ponte <cyphactor@gmail.com>
2022-04-22 15:24:56 -0400

    Add git-ps-rs formulae for v3.1.0
    Add git-ps-rs formulae so that people can instally git-ps-rs from
    Homebrew.
    
    I did a basic formulae for Rust command line tool built from source. I
    also hooked in the zsh and bash completion installation as well.

---
+++ Formula/git-ps-rs.rb
@@ -0,0 +1,23 @@
+class GitPsRs < Formula
+  desc "Git Patch Stack workflow CLI"
+  homepage "https://github.com/uptech/git-ps-rs"
+  url "https://github.com/uptech/git-ps-rs.git", tag: "3.1.0", revision: "6be74018117f8c593fb3a9dec5e34b0a646492be"
+  head "https://github.com/uptech/git-ps-rs.git"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    # Completion scripts and manpage are generated in the crate's build
+    # directory, which includes a fingerprint hash. Try to locate it first
+    out_dir = Dir["target/release/build/gps-*/out"].first
+    # man1.install "#{out_dir}/gps.1"
+    bash_completion.install "#{out_dir}/gps.bash"
+    zsh_completion.install "#{out_dir}/_gps"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gps --version")
+  end
+end
