ee9893ac4f253b602bea567577a3d7ff274a10b3
Drew De Ponte <cyphactor@gmail.com>
2022-04-27 17:34:44 -0400

    Release git-ps-rs v3.3.0 and remove binary bottles
    Release git-ps-rs v3.3.0 and remove the binary bottles because I don't
    think they are providing that much value since we have to have gpgme as
    a runtime dependency because we haven't figured out how to statically
    link gpgme as a dependency. If we figure that out then I will likely
    bring the binary bottles back. Also if more of our users have to have
    rust installed on their system they are closer to being able to
    contribute.

--- Formula/git-ps-rs.rb
+++ Formula/git-ps-rs.rb
@@ -1,15 +1,8 @@
 class GitPsRs < Formula
   desc "Git Patch Stack workflow CLI"
   homepage "https://github.com/uptech/git-ps-rs"
-  url "https://github.com/uptech/git-ps-rs.git", tag: "3.2.0", revision: "584ae9e48c5413e73af865e3799a90688655d23f"
+  url "https://github.com/uptech/git-ps-rs.git", tag: "3.3.0", revision: "6a092159a4e780108ef881fcdc8d988671b83007"
   head "https://github.com/uptech/git-ps-rs.git"
-
-  bottle do
-    root_url "https://github.com/uptech/homebrew-oss/raw/master/Formula"
-    rebuild 1
-    sha256 cellar: :any, monterey: "3d57b6362d96a15def0380eca34899fb9832d086bdabe7cef9f5b305c60de728"
-    sha256 cellar: :any, arm64_monterey: "5bc6c428fc7c6b558435df53bcddbded6359e35b18d638099b4000b91df5bdda"
-  end
 
   depends_on "rust" => :build
   depends_on "gpgme"
