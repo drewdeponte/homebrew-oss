98ed3b99b2a4b9f162aff486e3137aa21bff33b4
Drew De Ponte <cyphactor@gmail.com>
2022-07-18 15:50:30 -0400

    Release v0.1.2 of git-ticket-summary
    ps-id: 42d8d35c-d33d-4cb2-8119-c1b179e16ccf

--- Formula/git-ticket-summary.rb
+++ Formula/git-ticket-summary.rb
@@ -1,7 +1,7 @@
 class GitTicketSummary < Formula
   desc "Git Ticket Summary generator CLI extension for Git"
   homepage "https://github.com/uptech/git-ticket-summary"
-  url "https://github.com/uptech/git-ticket-summary.git", tag: "0.1.1", revision: "1b5c8336b365d7a39fb3ca6cb93a0f1e33cef67f"
+  url "https://github.com/uptech/git-ticket-summary.git", tag: "0.1.2", revision: "ceffd0875888db64f0b53b5421a333c1d12d5d88"
   head "https://github.com/uptech/git-ticket-summary.git"
 
   def install
