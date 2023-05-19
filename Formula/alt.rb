49ee9afc190039c80677f8d26e6c9a1f69fd3f21
Drew De Ponte <cyphactor@gmail.com>
2023-05-19 14:53:32 -0400

    Release alt v4.1.0

--- Formula/alt.rb
+++ Formula/alt.rb
@@ -1,7 +1,7 @@
 class Alt < Formula
   desc "Command-line utility to find alternate files"
   homepage "https://github.com/uptech/alt"
-  url "https://github.com/uptech/alt.git", tag: "v4.0.0", revision: "86ae6ca55db8f779867f907702363f5db39a1a34"
+  url "https://github.com/uptech/alt.git", tag: "v4.1.0", revision: "b075a2603ddd23fc96c2a54df55547c1723e9e86"
   head "https://github.com/uptech/alt.git"
 
   depends_on "rust" => :build
