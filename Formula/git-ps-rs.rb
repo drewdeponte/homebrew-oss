963de916dd441701f31979ef5ac3bc3ec25f51c7
Drew De Ponte <cyphactor@gmail.com>
2024-01-12 13:55:10 -0500

    Fix syntax issue in git-ps-rs 7.0.0 formula

--- Formula/git-ps-rs.rb
+++ Formula/git-ps-rs.rb
@@ -12,8 +12,7 @@ class GitPsRs < Formula
     # Completion scripts are generated in the crate's build
     # directory, which includes a fingerprint hash. Try to locate it first
     out_dir = Dir["target/release/build/gps-*/out"].first
-    man1.install "#{out_dir}/gps-branch.1", "#{out_dir}/gps-fetch.1", "#{out_dir}/gps-checkout.1", "#{out_dir}/gps-help.1", "#{out_dir}/gps-integrate.1", "#{out_dir}/gps-isolate.1"
-    "#{out_dir}/gps-list.1", "#{out_dir}/gps-pull.1", "#{out_dir}/gps-rebase.1", "#{out_dir}/gps-request-review.1", "#{out_dir}/gps-sha.1", "#{out_dir}/gps-show.1", "#{out_dir}/gps.1"
+    man1.install "#{out_dir}/gps-branch.1", "#{out_dir}/gps-fetch.1", "#{out_dir}/gps-checkout.1", "#{out_dir}/gps-help.1", "#{out_dir}/gps-integrate.1", "#{out_dir}/gps-isolate.1", "#{out_dir}/gps-list.1", "#{out_dir}/gps-pull.1", "#{out_dir}/gps-rebase.1", "#{out_dir}/gps-request-review.1", "#{out_dir}/gps-sha.1", "#{out_dir}/gps-show.1", "#{out_dir}/gps.1"
     bash_completion.install "#{out_dir}/gps.bash"
     zsh_completion.install "#{out_dir}/_gps"
   end
