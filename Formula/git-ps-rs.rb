884f31a13cd18b540458b88bc2b14489e127ffd2
Drew De Ponte <cyphactor@gmail.com>
2024-01-12 13:50:57 -0500

    Release git-ps-rs v7.0.0

--- Formula/git-ps-rs.rb
+++ Formula/git-ps-rs.rb
@@ -1,11 +1,10 @@
 class GitPsRs < Formula
   desc "Git Patch Stack workflow CLI"
   homepage "https://github.com/uptech/git-ps-rs"
-  url "https://github.com/uptech/git-ps-rs.git", tag: "6.9.0", revision: "8628560a965cc64d9195c19fdb16a3be476deb33"
+  url "https://github.com/uptech/git-ps-rs.git", tag: "7.0.0", revision: "070f2f1d3c0940a05b5f31f1fe7b6eb4d039d934"
   head "https://github.com/uptech/git-ps-rs.git"
 
   depends_on "rust" => :build
-  depends_on "gpgme"
 
   def install
     system "cargo", "install", *std_cargo_args
@@ -13,9 +12,8 @@ class GitPsRs < Formula
     # Completion scripts are generated in the crate's build
     # directory, which includes a fingerprint hash. Try to locate it first
     out_dir = Dir["target/release/build/gps-*/out"].first
-    man1.install "doc/gps-add.1"
-    # exmaple of installing multiple man pages
-    # man1.install "doc/gps-add.1", "doc/gps.1" 
+    man1.install "#{out_dir}/gps-branch.1", "#{out_dir}/gps-fetch.1", "#{out_dir}/gps-checkout.1", "#{out_dir}/gps-help.1", "#{out_dir}/gps-integrate.1", "#{out_dir}/gps-isolate.1"
+    "#{out_dir}/gps-list.1", "#{out_dir}/gps-pull.1", "#{out_dir}/gps-rebase.1", "#{out_dir}/gps-request-review.1", "#{out_dir}/gps-sha.1", "#{out_dir}/gps-show.1", "#{out_dir}/gps.1"
     bash_completion.install "#{out_dir}/gps.bash"
     zsh_completion.install "#{out_dir}/_gps"
   end
